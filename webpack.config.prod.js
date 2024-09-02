f2cd99accea5d4c736b5cebcc4c727ab0d4e1661
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2024-09-02 13:09:25 +0200

    `dev` and `prod` bundles are built in the same folder

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,7 @@
-const path = require('path');
 const devConfig = require('./webpack.config.dev');
-const { getCopyPlugin } = require('./webpack.utils');
-
-const buildPath = path.resolve(__dirname, 'build');
 
 module.exports = {
   ...devConfig,
   mode: 'production',
   devtool: undefined,
-  output: {
-    ...devConfig.output,
-    path: path.resolve(buildPath, 'js')
-  },
-  plugins: [getCopyPlugin(buildPath)]
 };
