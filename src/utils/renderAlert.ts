a6fb88d43688024ddd9a4cafbe0e765964835fa3
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2025-04-07 12:46:10 +0200

    Add `renderAlert` utility function

---
+++ src/utils/renderAlert.ts
@@ -0,0 +1,33 @@
+const id = 'responsiveOneCognizantAlert';
+
+const onDismiss = () => {
+  document.getElementById(id)?.remove();
+}
+
+const renderAlert = () => {
+  const wrapper = document.createElement('div');
+  wrapper.className = 'Alert';
+  wrapper.id = id;
+
+  const wrapperContent = document.createElement('div');
+  wrapperContent.className = 'Alert__Content';
+  wrapperContent.innerHTML = '';
+  wrapperContent.innerHTML += '<p class="Alert__Text Alert__Description">Enhanced by:</p>';
+  wrapperContent.innerHTML += '<p class="Alert__Text Alert__Title">Responsive OneCognizant</p>';
+
+  const wrapperBtns = document.createElement('div');
+  wrapperBtns.className = 'Alert__Buttons';
+
+  const dismissBtn = document.createElement('button');
+  dismissBtn.className = 'Alert__Button';
+  dismissBtn.innerHTML = 'x';
+  dismissBtn.onclick = onDismiss;
+
+  wrapperBtns.append(dismissBtn);
+  wrapper.append(wrapperContent);
+  wrapper.append(wrapperBtns);
+
+  document.body.append(wrapper);
+}
+
+export default renderAlert;
