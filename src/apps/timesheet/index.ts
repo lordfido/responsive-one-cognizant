c0a9abf3f64a217ebd701c0a4111c1f596e64a75
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2025-04-07 12:52:00 +0200

    Don't load `ROC`'s `Alert` on `timesheet`'s `iframe`

--- src/apps/timesheet/index.ts
+++ src/apps/timesheet/index.ts
@@ -1,4 +1,5 @@
 import { useAppInit } from '../../hooks';
+import renderAlert from '../../utils/renderAlert';
 import './timesheet';
 
 // Require the image, so it's available on `dist` folder
@@ -7,7 +8,12 @@ const bg = require('../../assets/logo_inverted.svg');
 const init = () => {
   useAppInit({
     frameName: 'timesheet',
-    handleOnLoad: () => {}
+    handleOnLoad: () => {
+      const appIframe = document.querySelector('iframe');
+      if (!!appIframe) {
+        renderAlert();
+      }
+    }
   });
 };
 
