68ced29c910fd85fb3501c266d611c1e834f712f
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2025-04-07 11:56:59 +0200

    Remove empty/dead code

--- src/apps/associate360/index.ts
+++ src/apps/associate360/index.ts
@@ -1,27 +1,10 @@
-import { DEBUG_RECURSIVE } from '../../constants';
 import { useAppInit } from '../../hooks';
-import { createDebouncedInterval, log } from '../../utils';
 import './associate360';
-
-const debounce = createDebouncedInterval();
-
-const removeHardcodedHeights = () => {};
-
-const removeHardcodedWidths = () => {};
 
 const init = () => {
   useAppInit({
     frameName: 'associate360',
-    handleOnLoad: () => {
-      debounce(() => {
-        if (DEBUG_RECURSIVE) {
-          log('removeHardcodedSizes');
-        }
-
-        removeHardcodedHeights();
-        removeHardcodedWidths();
-      });
-    }
+    handleOnLoad: () => {}
   });
 };
 
--- src/apps/liveSupport/index.ts
+++ src/apps/liveSupport/index.ts
@@ -1,36 +1,10 @@
-import { DEBUG_RECURSIVE } from '../../constants';
 import { useAppInit } from '../../hooks';
-import { createDebouncedInterval, log } from '../../utils';
 import './liveSupport';
-
-const debounce = createDebouncedInterval();
-
-const removeHardcodedHeights = () => {};
-
-const removeHardcodedWidths = () => {};
 
 const init = () => {
   useAppInit({
     frameName: 'liveSupport',
-    handleOnLoad: () => {
-      debounce(() => {
-        if (DEBUG_RECURSIVE) {
-          log('removeHardcodedSizes');
-        }
-
-        removeHardcodedHeights();
-        removeHardcodedWidths();
-      });
-
-      log('Setting the timeout');
-      setTimeout(() => {
-        const list = document.querySelector(
-          '.wrapper-autoComplete > .autoCompleteWrapper'
-        );
-
-        log(list?.innerHTML);
-      }, 10000);
-    }
+    handleOnLoad: () => {}
   });
 };
 
